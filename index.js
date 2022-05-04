2989c856798cd8fdb2cfd92b408f36d18fd4236f
Julkarnain <72872788+julkar28@users.noreply.github.com>
2022-05-04 23:15:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-// import qrcode from 'qrcode-terminal'
-// import ww from 'whatsapp-web.js'
-// const { Client, LocalAuth, MessageMedia } = ww
-
-// const client = new Client({
-//     authStrategy: new LocalAuth()
-// })
-// client.on('qr', qr => {
-//     qrcode.generate(qr, {small: true})
-// })
-
-// client.on('ready', () => {
-//     console.log('Client ready!')
-// })
-
-// client.initialize()
-
 // client.on('message', async (msg) => {
 //     const m = await msg.downloadMedia()
 //     console.log(m.mimetype)
@@ -24,12 +7,24 @@
 import express from 'express'
 import axios from 'axios'
 const app = express()
+import qrcode from 'qrcode-terminal'
+import ww from 'whatsapp-web.js'
+const { Client, LocalAuth, MessageMedia } = ww
+const client = new Client({
+    authStrategy: new LocalAuth()
+})
 app.get("/", (req, res) => {
     res.json({})
 })
 app.listen(3000, () => {
     console.log("Running on port 3000")
-    setInterval(() => {
-        axios.get("https://localhost:3000")
-    }, 60000)
+    client.on('qr', qr => {
+        qrcode.generate(qr, {small: true})
+    })
+
+    client.on('ready', () => {
+        console.log('Client ready!')
+    })
+
+    client.initialize()
 })
